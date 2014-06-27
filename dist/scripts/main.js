3548264487083f751a0138a418be2ce51a1add50
Zachary Sherman <zksherm@gmail.com>
2014-06-27 13:57:27 -0400

    more ui stuff

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -11,6 +11,7 @@ function loadTemplate(data, callback) {
       }
   };
   req.send(null);
+  console.log(data);
 }
 
 function updateTemplate(data) {
@@ -23,6 +24,8 @@ function updateTemplate(data) {
   else {
     $('html').append(html);
   }
+  var logo_url = chrome.extension.getURL('src/images/logo.png');
+  $('.sg-extension .logo').css('background-image', 'url(' + logo_url + ')');
 }
 
 $.subscribe('retrieved', function(ev, data) {
@@ -34,79 +37,23 @@ $.subscribe('retrieved', function(ev, data) {
   }
 });
 
-$(function() {
-(function() {
-  (function(root, factory) {
-    if (typeof define === 'function' && define.amd) {
-      return define(['jquery', 'waypoints'], factory);
-    } else {
-      return factory(root.jQuery);
-    }
-  })(window, function($) {
-    var defaults, wrap;
 
-    defaults = {
-      wrapper: '<div class="sticky-wrapper" />',
-      stuckClass: 'stuck',
-      direction: 'down right'
-    };
-    wrap = function($elements, options) {
-      var $parent;
+// Do all the sticky scrolling
+$( document ).on( 'mousewheel DOMMouseScroll', '.sg-extension', function ( e ) {
+    var e0 = e.originalEvent,
+        delta = e0.wheelDelta || -e0.detail;
+    
+    this.scrollTop += ( delta < 0 ? 1 : -1 ) * 8;
+    e.preventDefault();
+});
 
-      $elements.wrap(options.wrapper);
-      $parent = $elements.parent();
-      return $parent.data('isWaypointStickyWrapper', true);
-    };
-    $.waypoints('extendFn', 'sticky', function(opt) {
-      var $wrap, options, originalHandler;
-
-      options = $.extend({}, $.fn.waypoint.defaults, defaults, opt);
-      $wrap = wrap(this, options);
-      originalHandler = options.handler;
-      options.handler = function(direction) {
-        var $sticky, shouldBeStuck;
-
-        $sticky = $(this).children(':first');
-        shouldBeStuck = options.direction.indexOf(direction) !== -1;
-        $sticky.toggleClass(options.stuckClass, shouldBeStuck);
-        $wrap.height(shouldBeStuck ? $sticky.outerHeight() : '');
-        if (originalHandler != null) {
-          return originalHandler.call(this, direction);
-        }
-      };
-      $wrap.waypoint(options);
-      return this.data('stuckClass', options.stuckClass);
-    });
-    return $.waypoints('extendFn', 'unsticky', function() {
-      var $parent;
-
-      $parent = this.parent();
-      if (!$parent.data('isWaypointStickyWrapper')) {
-        return this;
-      }
-      $parent.waypoint('destroy');
-      this.unwrap();
-      return this.removeClass(this.data('stuckClass'));
-    });
-  });
-
-}).call(this);
-
-console.log("STICKY");
-
-
-  // Do all the sticky scrolling
-  $( '.scrollable' ).bind( 'mousewheel DOMMouseScroll', function ( e ) {
-      var e0 = e.originalEvent,
-          delta = e0.wheelDelta || -e0.detail;
-      
-      this.scrollTop += ( delta < 0 ? 1 : -1 ) * 5;
-      e.preventDefault();
-  });
-console.log($.waypoint);
-$('.sg-extension nav')[0].waypoint('sticky');
-
-
+$('.sg-extension').on('click', 'ul#menu .tab a', function(e) {
+  var ref = $(this).attr('href');
+  console.log(ref);
+  $('.sg-extension .tab-content').hide();
+  $(ref).show();
+  $('.sg-extension ul#menu .selected').removeClass('selected');
+  $(this).addClass('selected');
 });
 var o = $({});
 
@@ -181,8 +128,8 @@ var BaseParser = function() {
 
     that.updateArtist = function(callback) {
         var artist = that.getCurrentArtist();
-        console.log(artist);
         if (artist && artist != that.current_artist) {
+            console.log(artist);
             that.current_artist = artist;
             callback(that.current_artist);
         }
@@ -190,17 +137,29 @@ var BaseParser = function() {
 
     that.getCurrentArtist = function() {
         var artist_div = that.getArtistDiv();
-        console.log(artist_div);
         if (!artist_div) return null;
-        return that.clean_artist(artist_div.text || artist_div.innerHTML);
+        return that.cleanArtist(artist_div.text || artist_div.innerHTML);
     };
 
-    that.clean_artist = function(artist) {
+    that.cleanArtist = function(artist) {
         return artist;
     };
 
-    that.valid_page = function() {
+    that.validPage = function() {
         return true;
+    };
+
+    that.selectArtist = function(artist_list) {
+        if (!artist_list) return;
+        for (var i = 0; i < artist_list.length; i++) {
+            function clean(title) {
+                return title.trim().toLowerCase().replace(/\s/g, '');;
+            }
+            if (clean(that.current_artist) == clean(artist_list[i].name)) {
+                console.log("Matched");
+                return artist_list[i];
+            }
+        }
     };
 
     return that;
@@ -211,12 +170,12 @@ var HypemParser = function() {
     that.artist_selector = "#player-nowplaying a";
     that.listing_div = $(".track_name .artist");
 
-    that.initButtons = function() {
-        $(".section-track").each(function(i, div) {
-            var tools = $(div).find(".tools")[0];
-            $(tools).prepend("<li class='playdiv' style='width: 26px; height : 26px; margin: 6px; background-color: red;'><a class='icon-toggle'></a></li>");
-        });
-    };
+    // that.initButtons = function() {
+    //     $(".section-track").each(function(i, div) {
+    //         var tools = $(div).find(".tools")[0];
+    //         $(tools).prepend("<li class='playdiv' style='width: 26px; height : 26px; margin: 6px; background-color: red;'><a class='icon-toggle'></a></li>");
+    //     });
+    // };
     return that;
 };
 
@@ -244,7 +203,7 @@ var YoutubeParser = function() {
         }, 500);
     };
 
-    that.valid_page = function() {
+    that.validPage = function() {
         var category = $("#eow-category a")[0].text;
         if (category == "Music") {
             return true;
@@ -252,7 +211,7 @@ var YoutubeParser = function() {
         return false;
     };
 
-    that.clean_artist = function(artist) {
+    that.cleanArtist = function(artist) {
         if (artist.indexOf("-") != -1) {
             return artist.split("-")[0].trim();
         }
@@ -265,6 +224,7 @@ var YoutubeParser = function() {
 var API = function() {
     var that = {};
     that.api_url = "http://api.seatgeek.com/2";
+    that.client_id = "MzkyODY1fDE0MDM4ODEwMDA";
 
     that.getArtistResults = function(artist, callback) {
         var url = that.api_url + "/performers?" + $.param({q : artist});
@@ -272,7 +232,7 @@ var API = function() {
         $.getJSON(url, function(data) {
             console.log(data);
             if (data.performers.length != 0) {
-                callback(data.performers[0]);
+                callback(data.performers);
             }
             else {
                 callback(null);
@@ -287,8 +247,27 @@ var API = function() {
         });
     };
 
+    that.getGeoEventResults = function(artist_id, callback) {
+        var url = that.api_url + "/events?" + $.param({"performers.id" : artist_id, "geoip" : true});
+        $.getJSON(url, function(data) {
+            callback(data);
+        });
+    };
+
+    that.getRelatedResults = function(artist_id, callback) {
+        var url = that.api_url + "/recommendations/performers?" + $.param({"performers.id" : artist_id, "client_id" : that.client_id});
+        console.log(url);
+        $.getJSON(url, function(data) {
+            callback(data);
+        }).fail(function(data) {
+            callback(data);
+        }
+        );
+    };
+
     return that;
 };
+
 
 var App = function(hostname) {
     var that = {};
@@ -296,6 +275,8 @@ var App = function(hostname) {
     that.api = new API();
     that.artist_data;
     that.event_data;
+    that.geo_event_data;
+    that.related_data;
 
     if (hostname == "www.rdio.com") {
         that.parser = new RdioParser();
@@ -311,7 +292,7 @@ var App = function(hostname) {
     }
 
     that.init = function() {
-        if (!that.parser.valid_page()) {
+        if (!that.parser.validPage()) {
             return;
         }
         that.parser.artistDivFinder(that.artistUpdated.bind(that));
@@ -320,23 +301,53 @@ var App = function(hostname) {
     };
 
     that.artistUpdated = function(artist) {
-        console.log("In app artist updated ", artist);
+        that.artist_data = that.event_data = that.related_data = that.geo_event_data = null;
         that.api.getArtistResults(artist, that.artistRetrieved.bind(that));
     };
 
-    that.artistRetrieved = function(artist_data) {
+    that.artistRetrieved = function(artist_list) {
+        var artist_data = that.parser.selectArtist(artist_list);
         if (artist_data) {
             that.artist_data = artist_data;
             that.api.getEventResults(artist_data.id, that.eventRetrieved);
+            that.api.getGeoEventResults(artist_data.id, that.geoEventRetrieved);
+            that.api.getRelatedResults(artist_data.id, that.relatedRetrieved);
         }
     };
 
     that.eventRetrieved = function(event_data) {
-        if (event_data) {
-            that.event_data = event_data;
-            $.publish('retrieved', {event: that.event_data, artist: that.artist_data});
-            console.log(event_data);
-        };
+        that.event_data = event_data;
+        if (!that.event_data) {
+            that.related_data = {err : "No event response"};
+        }
+        that.tryPublish();
+    };
+
+    that.geoEventRetrieved = function(event_data) {
+        that.geo_event_data = event_data;
+        if (!that.geo_event_data) {
+            that.related_data = {err : "No geo response"};
+        }
+        that.tryPublish();
+    };
+
+    that.relatedRetrieved = function(related_data) {
+        that.related_data = related_data;
+        if (!that.related_data) {
+            that.related_data = {err : "No related artists found"};
+        }
+        that.tryPublish();
+    }
+
+    that.tryPublish = function() {
+        console.log(that.artist_data);
+        console.log(that.event_data);
+        console.log(that.related_data);
+        console.log(that.geo_event_data);
+        if (!(that.artist_data && that.event_data && that.related_data && that.geo_event_data)) return;
+        var message = {all_events: that.event_data, local_events: that.geo_event_data, artist: that.artist_data, related: that.related_data};
+        console.log(message);
+        $.publish('retrieved', message);
     };
 
     return that;
